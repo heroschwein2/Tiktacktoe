4651939be4d8225df9eb08f64fd5f1fe78be2258
Heroschwein <newtari@aikq.de>
2025-01-16 20:54:00 +0100

    made it check all squares in the win functions

--- Tiktacktoe/Program.cs
+++ Tiktacktoe/Program.cs
@@ -14,19 +14,19 @@ namespace Tiktacktoe
             board.BoardSetUp();
             legalCheck Legal = new legalCheck();
             randomPlay AI = new randomPlay();
-            while(Legal.isLegal==true)
+            while (Legal.isLegal == true)
             {
                 Legal.square = Convert.ToInt32(Console.ReadLine());
                 Legal.CheckSquare(board);
                 board.array[Legal.square] = 1;
                 board.CheckWin(1);
                 board.remainingMoves--;
-                AI.randomMove(board);
+                AI.AIOutput(board);
                 board.array[AI.AISquare] = 2;
                 board.CheckWin(2);
                 board.remainingMoves--;
             }
-            
+
         }
     }
     class Board
@@ -63,7 +63,7 @@ namespace Tiktacktoe
                     array[combination[2]] == player)
                 {
                     win = 1;//Player won
-                    Console.WriteLine("Winner");
+                    
                     return true;
                 }
             }
@@ -77,7 +77,7 @@ namespace Tiktacktoe
         public bool isLegal = true;
         public void CheckSquare(Board board)
         {
-            
+
             if (board.win == 0)
             {
                 if (board.array[square] == 1 || board.array[square] == 2)
@@ -101,47 +101,154 @@ namespace Tiktacktoe
         public int AISquare;
         public int tries = 0;
         public bool aiwin;
+        public bool Playerwin;
+        public int AIWinsquare;
+        public int PlayerWinsquare;
         public void randomMove(Board board)
         {
             legalCheck Legal = new legalCheck();
-            Random random = new Random(); 
+            Random random = new Random();
             bool moveFound = false;
 
-
-            while (!moveFound)  
+            for (int i = 0; i <9; i++)
             {
-                Legal.square = random.Next(0, 9);  //randomly generate move
-                Legal.CheckSquare(board);  // Checking if move is legal
+                    Legal.square = allMoves[i];
+                    Legal.CheckSquare(board);// Checking if move is legal
+                    moveFound = false;
+                    if (Legal.isLegal)  // Move is legal
+                    {
 
-                if (Legal.isLegal)  // Move is legal
+                        board.array[Legal.square] = 2;
+                        board.CheckWin(2);
+                        if (board.CheckWin(2) == true)
+                        {
+
+                            board.array[Legal.square] = 0;
+                            tries = 0;
+                            AIWinsquare = Legal.square;
+                            aiwin = true;
+                            moveFound = true;
+                            break;
+                    }
+                        else if (tries == board.remainingMoves)//no winning move possible
+                        {
+
+                        aiwin = false;
+                            
+                            tries = 0;
+                            board.array[Legal.square] = 0;
+                            break;
+                            
+
+
+                        }
+                        else
+                        { //Not a winning move
+
+                            board.array[Legal.square] = 0;
+                            tries = tries + 1;
+
+
+                        }
+
+                    }
+                
+            }
+
+        }
+        public int[] allMoves = new int[9];
+        public void intallmove()
+        {
+            for (int i = 0; i < allMoves.Length; i++)
+            {
+                allMoves[i] = i;
+            }
+        }
+        public void PlayerWin(Board board)
+        {
+            legalCheck Legal = new legalCheck();
+            Random random = new Random();
+            bool moveFound = false;
+            for (int i=0; i<9;i++)
+            {
+            intallmove();
+
+            
+
+                    Legal.square = allMoves[i];  //randomly generate move
+                    Legal.CheckSquare(board);  // Checking if move is legal
+                    
+                    if (Legal.isLegal)
+                    {
+                        board.array[Legal.square] = 1;
+                        board.CheckWin(1);
+                        if (board.CheckWin(1) == true)
+                        {
+                            PlayerWinsquare = Legal.square;
+                            board.array[Legal.square] = 0;
+                            Playerwin = true;
+                            moveFound = true;
+                            tries = 0;
+                        }
+                        else if (tries == board.remainingMoves)
+                        {
+                            tries = 0;
+                            board.array[Legal.square] = 0;
+                            Playerwin = false;
+                            moveFound = true;
+                        }
+                        else
+                        {
+                            board.array[Legal.square] = 0;
+                            tries = tries + 1;
+
+                        }
+                    }
+                
+            }
+        }
+        public void AIOutput(Board board)
+        {
+            Random random = new Random();
+            legalCheck Legal = new legalCheck();
+            intallmove();
+            randomMove(board);
+            PlayerWin(board);
+            if (Playerwin)
+            {
+                if (aiwin)
                 {
-                    board.array[Legal.square] = 2;
-                    board.CheckWin(2);
-                    if (board.CheckWin(2)==true)
-                    {
-                        AISquare = Legal.square;
-                        Console.WriteLine(AISquare);
-                        Console.WriteLine("Winner:AI");
-                        moveFound = true;  // Ends if move is found
-                    }
-                    else if (tries == board.remainingMoves)//no winning move possible
-                    {
-                        AISquare = Legal.square;
-                        moveFound = true;
-                        tries = 0;
-                        Console.WriteLine(AISquare);
-
-                    }
-                    else { //Not a winning move
-                        
-                        board.array[Legal.square] = 0;
-                        tries = tries + 1;
-
-                    }
-
+                    AISquare = AIWinsquare;
+                }
+                else
+                {
+                    AISquare = PlayerWinsquare;
+                    board.win = 0;
                 }
             }
-            
+            else if (aiwin)
+            {
+                AISquare = AIWinsquare;
+                board.win = 0;
+            }
+            else
+            {
+                bool moveFound=false;
+                while (!moveFound)
+                {
+                    Legal.square = random.Next(0, 9);
+                    Legal.CheckSquare(board);
+                    if (Legal.isLegal)
+                    {
+                        AISquare = Legal.square;
+                        moveFound=true;
+                        break;
+                    }
+                }     
+                board.win = 0;
+            }
+            Console.WriteLine(AISquare);
         }
+
     }
 }
